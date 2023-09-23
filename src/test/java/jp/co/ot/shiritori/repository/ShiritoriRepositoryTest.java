b74c0724eff10dea6adf7de5a163a2b2879acc79
Keisuke Ota <k.ota109@outlook.com>
2023-09-23 22:49:56 +0900

    Repositoryのテストを実装

---
+++ src/test/java/jp/co/ot/shiritori/repository/ShiritoriRepositoryTest.java
@@ -0,0 +1,83 @@
+package jp.co.ot.shiritori.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.ot.shiritori.domain.request.ShiritoriEntryRequest;
+import jp.co.ot.shiritori.domain.request.ShiritoriWordRequest;
+import jp.co.ot.shiritori.domain.response.EntryIdResponse;
+import jp.co.ot.shiritori.domain.response.ShiritoriResultResponse;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ShiritoriRepositoryTest {
+
+	@Autowired
+	private ShiritoriRepository sut;
+	
+	@Test
+	@Ignore
+	public void entry_entryIdを保存できること() {
+		ShiritoriEntryRequest request = new ShiritoriEntryRequest();
+		request.setEntryId("testhoge30");
+		try {
+			sut.entry(request);
+		} catch (Exception e) {
+			System.out.println("============================");
+			System.out.println(e);
+			System.out.println("============================");
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void judge_ワードを取得できること() {
+		ShiritoriWordRequest request = new ShiritoriWordRequest();
+		request.setWord("うんこ");
+		try {
+			ShiritoriResultResponse actual = sut.judge("hogehoge", request);
+			assertEquals("うんこ", actual.getWord());
+		} catch (Exception e) {
+			System.out.println("============================");
+			System.out.println(e);
+			System.out.println("============================");
+		}
+	}
+
+	@Test
+	@Ignore
+	public void saveWord_キーワードを保存できること() {
+		ShiritoriWordRequest request = new ShiritoriWordRequest();
+		request.setWord("うんこ");
+		try {
+			sut.saveWord("testhoge30", request);
+		} catch (Exception e) {
+			System.out.println("============================");
+			System.out.println(e);
+			System.out.println("============================");
+		}
+	}
+	
+	@Test
+	@Ignore
+	public void getEntryId_EntryIdを取得できること() {
+		try {
+			EntryIdResponse actual = sut.getEntryId("testhoge30");
+			assertEquals("testhoge30", actual);
+		} catch (Exception e) {
+			System.out.println("============================");
+			System.out.println(e);
+			System.out.println("============================");
+		}
+	}
+	
+	
+	
+
+}
