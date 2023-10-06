4a91e477b702bb98fead7108c6cec58c0953e036
Keisuke Ota <k.ota109@outlook.com>
2023-10-07 02:11:36 +0900

    DDLを修正

--- document/ddl/shiritori.sql
+++ document/ddl/shiritori.sql
@@ -1,12 +1,11 @@
 DROP TABLE IF EXISTS training.shiritori;
 
-CREATE TABLE training.shiritori (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    entry varchar(30) NOT NULL,
-    word varchar(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci;
+CREATE TABLE `shiritori` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `entry` varchar(30) NOT NULL,
+  `word` varchar(100) NOT NULL,
+  `raw_word` varchar(100) NOT NULL,  -- Add the rawWord column
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
